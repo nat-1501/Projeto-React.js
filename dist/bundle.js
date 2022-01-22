6eeb60624ab1ad524dcdb8f1d510de69d071e27a
nat-1501 <natalisoares90@gmail.com>
2022-01-22 12:21:44 -0300

    configurando o webpack.config

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,7 +2,7 @@
 
 var _react = _interopRequireDefault(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+***********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function App() {
   return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello word");
