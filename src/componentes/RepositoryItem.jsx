9bdf7dbfec74dffc5a2d225339ba8f02005618c4
nat-1501 <natalisoares90@gmail.com>
2022-01-26 20:02:37 -0300

    conhecendo proprieadades

---
+++ src/componentes/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem() {
+    return (
+        <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessa repositorio
+                    </a>
+
+                </li>
+    );
+}
--- src/componentes/RepositoryList.jsx
+++ src/componentes/RepositoryList.jsx
@@ -1,49 +1,18 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repositoryName = 'unform2';
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
-
             <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessa repositorio
-                    </a>
-
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessa repositorio
-                    </a>
-
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessa repositorio
-                    </a>
-
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessa repositorio
-                    </a>
-
-                </li>
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
             </ul>
         </section>
     );
