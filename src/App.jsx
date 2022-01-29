9939781c618bcb1dcabeb4b3932b21e70f733b01
nat-1501 <natalisoares90@gmail.com>
2022-01-29 19:52:27 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './componentes/Counter';
 import { RepositoryList } from './componentes/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
